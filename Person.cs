9821d767412694e8e126af7e781e4cf0e264cfbf
Richi-Kodehode <richard25sfj@gmail.com>
2025-01-27 13:02:48 +0100

    updated notes

--- Person.cs
+++ Person.cs
@@ -1,13 +1,19 @@
 class Person
 {
+    // Datafield
     public String firstName;
     public String lastName;
+
+    // Constructor
+    // logics to make a new instance
     public Person(string firstName, string lastName)
     {
         
         this.firstName = firstName;
         this.lastName= lastName;
     }
+    // Methods
+    //What we can do with this type
     public String GetFullName() {
         return firstName + " " + lastName;
     }
